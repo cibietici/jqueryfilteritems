ebad4bf0bf7a2bd64119572a4d04f36f7deba409
carlo alberto burato <carloalberto.burato@gmail.com>
2020-02-21 21:33:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 $(document).on('click', '.sweet', function(){
-	$('li#id-Salt').hide(500);
-	$('li#id-Sweet').show(500);
+    $('li#id-Salt').hide(500);
+    $('li#id-Sweet').show(500);
 });
 $(document).on('click', '.salt', function(){
-	$('li#id-Salt').show(500);
-     $('li#id-Sweet').hide(500);      
+    $('li#id-Salt').show(500);
+    $('li#id-Sweet').hide(500);      
 });
 $(document).on('click', '.all', function(){
-	$('li#id-Salt').show(500);
-     $('li#id-Sweet').show(500);      
+    $('li#id-Salt').show(500);
+    $('li#id-Sweet').show(500);      
 });
